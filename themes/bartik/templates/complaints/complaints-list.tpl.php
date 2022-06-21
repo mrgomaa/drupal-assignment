e20272ddfd2a0fe1e0e897a1dd1ae579c5e8862d
Mohamed Gomaa <mgom3a@gmail.com>
2022-06-21 09:33:38 +0300

    refact: complaints module

--- themes/bartik/templates/complaints/complaints-list.tpl.php
+++ themes/bartik/templates/complaints/complaints-list.tpl.php
@@ -8,36 +8,29 @@ if (in_array('administrator', array_values($user->roles)) &&  in_array('moderato
 } else {
     // is not admin
 }
-// echo "hello complaints" ;
-// echo count($data);
-if(count($data) > 0)
-{
 
-if (in_array('Manager', array_values($user->roles))) {
-    echo "<table><tr><th>name</th><th>mail</th><th>phone</th><th>complaint type</th><th>Workflow</th><th>Action</th></tr>";
-} else {
-    echo "<table><tr><th>name</th><th>mail</th><th>phone</th><th>complaint type</th><th>Workflow</th></tr>";
-}
-    
-    foreach($data as $v)
-    {
+if (count($data) > 0) {
+    if (in_array('Manager', array_values($user->roles))) {
+        echo "<table><tr><th>name</th><th>mail</th><th>phone</th><th>complaint type</th><th>Workflow</th><th>Action</th></tr>";
+    } else {
+        echo "<table><tr><th>name</th><th>mail</th><th>phone</th><th>complaint type</th><th>Workflow</th></tr>";
+    }
+
+    foreach ($data as $v) {
         echo "<tr>";
         echo "<td>" . $v['name'] . "</td>";
         echo "<td>" . $v['mail'] . "</td>";
         echo "<td>" . $v['phone'] . "</td>";
         echo "<td>" . $v['complaint_type'] . "</td>";
         echo "<td>" . $v['workflow'] . "</td>";
-if (in_array('Manager', array_values($user->roles))) {
-    echo "<td><a href='". $base_url ."?q=node/". $v['edit'] ."/edit' >Edit</a></td>";
-} else {
-    
-}
-        
+        if (in_array('Manager', array_values($user->roles))) {
+            echo "<td><a href='" . $base_url . "?q=node/" . $v['edit'] . "/edit' >Edit</a></td>";
+        } else {
+        }
+
         echo "</tr>";
     }
-    echo "</table>";    
-}
-else
-{
+    echo "</table>";
+} else {
     echo "No records found";
-} 
+}
