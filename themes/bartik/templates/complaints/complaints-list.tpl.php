98bcd0047a384a95fb3490a56e9a033a920b6d98
Mohamed Gomaa <mgom3a@gmail.com>
2022-06-21 08:28:10 +0300

    refact complaint module * check access based on user roles * user field access hook * add some contributed modules

--- themes/bartik/templates/complaints/complaints-list.tpl.php
+++ themes/bartik/templates/complaints/complaints-list.tpl.php
@@ -1,18 +1,38 @@
 <h2>display complaints</h2>
 <?php
+global $base_url;
+global $user;
+
+if (in_array('administrator', array_values($user->roles)) &&  in_array('moderator', array_values($user->roles))) {
+    // is admin
+} else {
+    // is not admin
+}
 // echo "hello complaints" ;
 // echo count($data);
 if(count($data) > 0)
 {
+
+if (in_array('Manager', array_values($user->roles))) {
+    echo "<table><tr><th>name</th><th>mail</th><th>phone</th><th>complaint type</th><th>Workflow</th><th>Action</th></tr>";
+} else {
     echo "<table><tr><th>name</th><th>mail</th><th>phone</th><th>complaint type</th><th>Workflow</th></tr>";
+}
+    
     foreach($data as $v)
     {
         echo "<tr>";
-        echo "<td>" . $v['phone'] . "</td>";
+        echo "<td>" . $v['name'] . "</td>";
         echo "<td>" . $v['mail'] . "</td>";
         echo "<td>" . $v['phone'] . "</td>";
         echo "<td>" . $v['complaint_type'] . "</td>";
-        echo "<td>" . $v['workflow'] . "</td>"; 
+        echo "<td>" . $v['workflow'] . "</td>";
+if (in_array('Manager', array_values($user->roles))) {
+    echo "<td><a href='". $base_url ."?q=node/". $v['edit'] ."/edit' >Edit</a></td>";
+} else {
+    
+}
+        
         echo "</tr>";
     }
     echo "</table>";    
